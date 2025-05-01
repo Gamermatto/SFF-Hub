323f9685bcc916e061b439e6ff784552ce6bc942
Gamermatto <gamermatto@icloud.com>
2025-05-01 19:33:33 +0200

    SFF Hub

--- SFF-Hub.lua
+++ SFF-Hub.lua
@@ -1,6 +1,6 @@
 --if game.PlaceId == 13772394625 then
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Blade Ball",
