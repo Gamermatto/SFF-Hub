76113ca275bc64deaf06f5ff259dc51b28bed70d
Gamermatto <gamermatto@icloud.com>
2025-05-01 19:40:02 +0200

    SFF Hub

--- SFF-Hub.lua
+++ SFF-Hub.lua
@@ -55,12 +55,12 @@ Rayfield:Notify({
 
  local Slider = MainTab:CreateSlider({
    Name = "Walk Speed",
-   Range = {0, 200},
+   Range = {3, 200},
    Increment = 1,
    Suffix = "Speed",
    CurrentValue = 16,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-      game.Players.LocalPlayer.Character.Humanoid.Walkspeed = (Value)
+      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
    end,
 })
