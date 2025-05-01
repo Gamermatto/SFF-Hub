4b8df8440f569956c7294b233369e7b02b841245
Gamermatto <gamermatto@icloud.com>
2025-05-01 20:09:17 +0200

    .

--- SFF-Hub.lua
+++ SFF-Hub.lua
@@ -57,13 +57,14 @@ local MainSection = MainTab:CreateSection("Main")
 -- Quando vuoi mostrare la notifica, chiama la funzione
 showAchievementNotification()
 
-
- local Button = MainTab:CreateButton({
-    Name = "Fly",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/TV83kUPv", true))()
-    end,
- })
+local Toggle = Tab:CreateToggle({
+   Name = "Fly",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+      loadstring(game:HttpGet("https://pastebin.com/raw/TV83kUPv", true))()
+   end,
+})
 
  local Slider = MainTab:CreateSlider({
    Name = "WalkSpeed",
